889a0548562d00c88a18cf2afc00d4b3f9044c64
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-08-14 20:25:08 +0300

    done

---
+++ GcdTask/IntegerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GcdTask
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue]  by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int FindGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException($"numbers {a} and {b} are 0");
+            }
+            else if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException($"{a} and {b} are int.MinValue");
+            }
+            
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
+
+            return a + b;
+        }
+    }
+}
